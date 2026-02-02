6cd6aca0796505a530e1fb98b6d63f41ee965b20
palatul211207-maker <palatul211207@gmail.com>
2026-02-02 16:32:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
 const auth = firebase.auth();
 
 function register() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
+  const email = email.value;
+  const password = password.value;
   auth.createUserWithEmailAndPassword(email, password)
-    .then(() => alert("Registered Successfully"))
-    .catch(err => alert(err.message));
+    .then(() => alert("Registered"))
+    .catch(e => alert(e.message));
 }
 
 function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  auth.signInWithEmailAndPassword(email, password)
-    .then(() => alert("Login Successful"))
-    .catch(err => alert(err.message));
+  auth.signInWithEmailAndPassword(email.value, password.value)
+    .catch(e => alert(e.message));
 }
 
 function logout() {
   auth.signOut();
 }
 
-function showApp() {
-  document.getElementById("loginBox").style.display = "none";
-  document.getElementById("quoteBox").style.display = "block";
-}
-
 auth.onAuthStateChanged(user => {
-  if (user) showApp();
+  if (user) {
+    loginBox.style.display = "none";
+    quoteBox.style.display = "block";
+  }
 });
 
 const quotes = [
-  "Success comes to those who work hard.",
-  "Never give up.",
-  "Believe in yourself.",
-  "Dream big and dare to fail."
+  "Success comes to those who work hard",
+  "Never give up",
+  "Believe in yourself",
+  "Dream big"
 ];
 
 function newQuote() {
-  document.getElementById("quote").innerText =
-    quotes[Math.floor(Math.random() * quotes.length)];
+  quote.innerText = quotes[Math.floor(Math.random() * quotes.length)];
 }
